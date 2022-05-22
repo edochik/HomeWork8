249a81fd6b6fda15bca44006693b3260e1f5c353
edochik <edoch@yandex.ru>
2022-05-22 13:37:10 +0300

    add information Task2

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 1: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+*/
--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Console.WriteLine("Среднее: " + avg);
+
+double[] CreateArray(double[] array)
+{
+    for (double i = 0; i < array.Length; i++)
+    {
+        array[(int)i] = new Random().NextDouble()*0.1;
+    }
+    return array;
+}
+
+void PrintArray(double[] array)
+
+{
+    for (double i = 0; i < array.Length; i++)
+    {
+        Console.Write($" {array[(int)i]}");
+    }
+}
+
+double[] array = new double[6];
+
+int AveregNumberArray(int[] array)
+{
+    int resultA = 0;
+    int resultB = 0;
+    int avg = 0;
+    // for (int i = 0; i < array.Length; i++)
+    // {
+        // if (array[i] > -1) resultA += 1;
+    // }
+    for (int j = 0; j < array.Length; j++)
+    {
+        if (array[j] > 0) resultB += array[j];
+    }
+    return avg = resultB / array.Length;
+}
+
+CreateArray(array);
+PrintArray(array);
+Console.WriteLine();
+Console.Write(AveregNumberArray(array));
